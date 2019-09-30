23d4d9ff9ee1aacea10e0deee81dac4422d210a1
HemulGM <alinvip22@gmail.com>
2019-09-30 08:06:40 +0700

    Update

--- Weather.Main.pas
+++ Weather.Main.pas
@@ -8,7 +8,8 @@ uses
   System.Net.HttpClient, Vcl.StdCtrls, IPPeerClient, REST.Client,
   Data.Bind.Components, Data.Bind.ObjectScope, Weather.Classes, System.JSON,
   Vcl.ExtCtrls, Vcl.Imaging.pngimage, System.ImageList, Vcl.ImgList, HGM.Button,
-  Direct2D, D2D1, acPNG;
+  Direct2D, D2D1, System.Generics.Collections, Vcl.WinXCtrls,
+  HGM.Controls.Labels, HGM.Controls.Labels.Base;
 
 type
   TFormWeather = class(TForm)
@@ -26,10 +27,6 @@ type
     ImageList32: TImageList;
     ButtonFlatUpdate: TButtonFlat;
     ImageList24: TImageList;
-    ButtonFlat1: TButtonFlat;
-    ButtonFlat2: TButtonFlat;
-    ButtonFlat3: TButtonFlat;
-    ButtonFlat4: TButtonFlat;
     PanelSettings: TPanel;
     EditCity: TEdit;
     Label1: TLabel;
@@ -37,6 +34,7 @@ type
     ButtonFlatSetOk: TButtonFlat;
     TimerUpdate: TTimer;
     LabelError: TLabel;
+    hLabel1: ThLabel;
     procedure FormCreate(Sender: TObject);
     procedure ButtonFlatUpdateClick(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
@@ -48,10 +46,12 @@ type
     procedure ButtonFlatSetOkClick(Sender: TObject);
     procedure ButtonFlatSettingsClick(Sender: TObject);
     procedure TimerUpdateTimer(Sender: TObject);
+    procedure FormMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
   private
     FWeather: TWeather;
     FCity: string;
     FRotate: Integer;
+    procedure CreateParams(var Params: TCreateParams); override;
   public
     { Public declarations }
   end;
@@ -150,6 +150,12 @@ begin
   FRotate := Round(360 - FWeather.wind.deg);
 end;
 
+procedure TFormWeather.CreateParams(var Params: TCreateParams);
+begin
+  inherited;
+  Params.Style := Params.Style or WS_BORDER or WS_THICKFRAME;
+end;
+
 procedure TFormWeather.FormActivate(Sender: TObject);
 begin
   ButtonFlatSettings.Visible := True;
@@ -164,6 +170,8 @@ begin
   PanelSettings.Hide;
   PanelSettings.BoundsRect := ClientRect;
   ButtonFlatUpdateClick(nil);
+  Left := Screen.Width - ClientWidth;
+  Top := 0;
 end;
 
 procedure TFormWeather.FormDeactivate(Sender: TObject);
@@ -177,25 +185,71 @@ begin
   FWeather.Free;
 end;
 
+procedure TFormWeather.FormMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  ReleaseCapture;
+  SendMessage(Handle, WM_SYSCOMMAND, 61458, 0);
+end;
+
 procedure TFormWeather.FormPaint(Sender: TObject);
 var
   D2: TDirect2DCanvas;
   ICO: TIcon;
   Pt: TPoint;
+  R: TRect;
 begin
   with TDirect2DCanvas.Create(Canvas, ClientRect) do
   begin
     BeginDraw;
-    Pt := Point(LabelWind.Left + LabelWind.Width, LabelWind.Top - 8);
-    RenderTarget.SetTransform(TD2D1Matrix3x2F.Rotation(90 - FRotate, Pt.X + 12, Pt.Y + 12));
+    Pt := Point(LabelWind.Left + LabelWind.Width, LabelWind.Top - 6);
+    RenderTarget.SetTransform(TD2D1Matrix3x2F.Rotation(180 + FRotate, Pt.X + 12, Pt.Y + 12));
     ICO := TIcon.Create;
-    ImageList24.GetIcon(4, ICO);
+    ImageList24.GetIcon(7, ICO);
     Draw(Pt.X, Pt.Y, ICO);
     ICO.Free;
     RenderTarget.SetTransform(TD2DMatrix3x2F.Identity);
+
+    Brush.Color := Color;
+    Pen.Color := clGray;
+    Pen.Width := 3;
+    //
+    R := TRect.Create(TPoint.Create(10, 80), 30, 30);
+    Ellipse(R);
+    //
+    R.Inflate(-5, 0);
+    R.Top := 10;
+    R.Bottom := 100;
+    RoundRect(R, 20, 20);
+    //
+    Pen.Color := Color;
+    R := TRect.Create(TPoint.Create(10, 80), 30, 30);
+    R.Inflate(-3, -3);
+    Ellipse(R);
+    //
+    Brush.Color := $008A8AFF;
+    Pen.Color := $008A8AFF;
+    R := TRect.Create(TPoint.Create(10, 80), 30, 30);
+    R.Inflate(-5, -5);
+    Ellipse(R);
+    //
+    R := TRect.Create(TPoint.Create(10, 80), 30, 30);
+    //
+    Pen.Width := 1;
+    R := TRect.Create(TPoint.Create(10, 80), 30, 30);
+    R.Inflate(-10, 0);
+    //R.Top := 20;
+    //R.Top := 80;
+    R.Top := 50;
+    R.Bottom := 100;
+    R.Width := R.Width + 1;
+    Rectangle(R);
     EndDraw;
     Free;
-  end;
+  end;                    //6, 30
+  ImageList24.Draw(Canvas, LabelWind.Left - 30, LabelWind.Top - 6, 1, True);
+  ImageList24.Draw(Canvas, LabelPressure.Left - 30, LabelPressure.Top - 6, 2, True);
+  ImageList24.Draw(Canvas, LabelHumidity.Left - 30, LabelHumidity.Top - 6, 0, True);
+  ImageList24.Draw(Canvas, LabelCloudiness.Left - 30, LabelCloudiness.Top - 6, 3, True);
 end;
 
 procedure TFormWeather.FormResize(Sender: TObject);
@@ -205,7 +259,8 @@ end;
 
 procedure TFormWeather.TimerUpdateTimer(Sender: TObject);
 begin
-  if PanelSettings.Visible then Exit;
+  if PanelSettings.Visible then
+    Exit;
   ButtonFlatUpdateClick(nil);
 end;
 
